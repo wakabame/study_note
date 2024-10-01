b3cc8cc01fdb8c3b3114d02bed003e666f32b3d7
Yuki Kimura <35513518+wakabame@users.noreply.github.com>
2024-10-01 17:39:57 +0900

    Update sample.py via github UI
    UI から操作したときにどうなるかの確認

--- study_note/sample.py
+++ study_note/sample.py
@@ -14,3 +14,20 @@ def sample_add_int(x: int, y: int) -> int:
         ２つの値の合計
     """
     return x + y
+
+def sample_product_int(x: int, y: int) -> int:
+    """２つの整数の積を返します
+
+    Parameters
+    ----------
+    x : int
+        整数そのいち
+    y : int
+        整数そのに
+
+    Returns
+    -------
+    int
+        ２つの値の積
+    """
+    return x * y
