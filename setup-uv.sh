9234b2f97ee25d8e064bf8860c33b185f9912096
wakabame <wakabame89@gmail.com>
2025-08-15 10:30:39 +0000

    uv は常に最新版を使用することにする

--- setup-uv.sh
+++ setup-uv.sh
@@ -1,5 +1,3 @@
 #/bin/bash
-# uv のバージョン固定
-UV_VERSION=0.5.9
-curl -LsSf https://astral.sh/uv/$UV_VERSION/install.sh | sh
+curl -LsSf https://astral.sh/uv/install.sh | sh
 echo 'eval "$(uv generate-shell-completion bash)"' >> ~/.bashrc
